d8404cc055381ff5751856e14a03a4b0115ed88b
DUst <udv@ec-asu.ru>
2023-07-18 08:52:28 +0500

    HW3_4

--- Java_OOP/hero/BaseHero.java
+++ Java_OOP/hero/BaseHero.java
@@ -1,105 +1,166 @@
 package hero;
 
+import java.util.ArrayList;
 
-public abstract class BaseHero implements BaseHeroInterface {
-    protected String name;
+public abstract class BaseHero implements HeroInterface, Comparable<BaseHero> {
+    public String name;
+    public String type;
 
-    protected int strength;
-    protected int dexterity;
-    protected int intelligence;
-    protected int endurance;
+    public int health;
+    public int healthMax;
 
-    protected int health;
-    protected int healthMax;
+    public int attack;
+    public int defense;
+    public int speed;
+
+    public int damageMin;
+    public int damageMax;
+
+    public int posX;
+    public int posY;
+
+    public int state;
+
 
     // Полный конструктор
-    public BaseHero(String name,
-                    int strength, int dexterity, int intelligence, int endurance,
-                    int health, int healthMax) {
-        this.name         = name;
+    public BaseHero( String name,
+                     int health, int attack, int defense, int speed,
+                     int damageMin, int damageMax, int posX, int posY ) {
+        this.name       = name;
+        this.type       = "";
 
-        this.strength     = strength;
-        this.dexterity    = dexterity;
-        this.intelligence = intelligence;
-        this.endurance    = endurance;
+        this.health     = health;
+        this.healthMax  = health;
 
-        this.health       = health;
-        this.healthMax    = healthMax;
+        this.attack     = attack;
+        this.defense    = defense;
+        this.speed      = speed;
+
+        this.damageMin = damageMin;
+        this.damageMax = damageMax;
+
+        this.posX      = posX;
+        this.posY      = posY;
+
+        this.state      = 1;
     }
 
-    // Краткий конструктор, через this(...)
-    public BaseHero( String name, int strength, int health ) {
-        this(
-            name,
-            strength,
-            0,
-            0,
-            0,
-            health,
-            health );
-    }
-
-    // Пустой конструктор, через this(...)
-    public BaseHero() {
-        this( "unknown", 1, 0, 0, 0, 5, 5 );
+    // Полный конструктор, без разброса в уроне
+    public BaseHero( String name,
+                     int health, int attack, int defense, int speed,
+                     int damage, int posX, int posY ) {
+        this( name, health, attack, defense, speed, damage, damage, posX, posY );
     }
 
 
     // Геттеры, для изменения значений приватных полей
-    @Override
-    public String getName()      { return name; }
-    public int getStrength()     { return strength; }
-    public int getDexterity()    { return dexterity; }
-    public int getIntelligence() { return intelligence; }
-    public int getEndurance()    { return endurance; }
-    public int getHealth()       { return health; }
-    public int getHealthMax()    { return healthMax; }
+    public String getName  () { return this.name;      }
+    public int getHealth   () { return this.health;    }
+    public int getHealthMax() { return this.healthMax; }
+    public int getAttack   () { return this.attack;    }
+    public int getDefense  () { return this.defense;   }
+    public int getSpeed    () { return this.speed;     }
+    public int getDamageMin() { return this.damageMin; }
+    public int getDamageMax() { return this.damageMax; }
+    public int getPosX     () { return this.posX;      }
+    public int getPosY     () { return this.posY;      }
+    public int getState    () { return this.state;     }
+    public Point2D getPos  () { return new Point2D(posX, posY); }
+
 
     // Сеттеры, для получения значений приватных полей
-    public void setName(         String name )      { this.name         = name; }
-    public void setStrength(     int strength )     { this.strength     = strength; }
-    public void setDexterity(    int dexterity )    { this.dexterity    = dexterity; }
-    public void setIntelligence( int intelligence ) { this.intelligence = intelligence; }
-    public void setEndurance(    int endurance )    { this.endurance    = endurance; }
-    public void setHealth(       int health )       { this.health       = health; }
-    public void setHealthMax(    int health )       { this.healthMax    = health; }
+    public void setName     ( String name   ) { this.name      = name;      }
+    public void setAttack   ( int attack    ) { this.attack    = attack;    }
+    public void setDefense  ( int defense   ) { this.defense   = defense;   }
+    public void setSpeed    ( int speed     ) { this.speed     = speed;     }
+    public void setDamageMin( int damageMin ) { this.damageMin = damageMin; }
+    public void setDamageMax( int damageMax ) { this.damageMax = damageMax; }
+    public void setHealth   ( int health    ) { this.health    = health;    }
+    public void setState    ( int state     ) { this.state     = state;     }
 
+    // Сеттеры, для позиционирования
+    public void setPos      ( Point2D pos ) { this.posX = pos.posX; this.posY = pos.posY; }
+    public void setPosX     ( int posX    ) { setPos( new Point2D( posX, this.posY ) ); }
+    public void setPosY     ( int posY    ) { setPos( new Point2D( this.posX, posY ) ); }
 
-    // Действие - Смерть
-    public void die() {
-        setHealth( 0 );
-        setHealthMax( 0 );
+    // Рассчитываем урон по цели, минимальный урон == 1
+    public int getDamage( int heroDefense ) {
+        int damage = heroDefense - this.attack - ( int )( ( damageMin + damageMax ) / 2 );
+        if ( damage < 0 )
+            return damage * -1;
+        return 1;
     }
 
-    // Действие - Ранение
-    public void takeDamage( int damage ) {
-        if ( this.health > damage ) {
-            this.health -= damage;
-            System.out.println( this.name + " take damage -" + damage + "hp" );
-        } else {
-            this.die();
-            System.out.println( this.name + " has been defeated!" );
+
+
+    public void doStep( ArrayList<BaseHero> enemy ) {
+        doTurn( enemy );
+    }
+
+    public void doTurn( ArrayList<BaseHero> enemy ) {
+        System.out.println( "Даже не знаю, что вам ответить..." );
+    }
+
+    // Действие Атака
+    public void doAttack( BaseHero hero, int damage ) {
+        hero.doDamage( damage );
+    }
+
+
+    // Получаем урон - см. HeroInterface, он реализован там... но не переносится... х.з. эту Java
+    public void doDamage( float damage ) {
+            setHealth( getHealth() - (int)damage );
+        if ( getHealth() <= 0 ) {
+            setHealth( 0 );
+            setState ( -1 );
+            System.out.println( this + " персонаж погиб" );
         }
     }
 
-    // Действие - Атака
-    public void attack( BaseHero target ) {
-        int damage = this.strength * 1;
-        target.takeDamage( damage );
-        System.out.println( this.name + " attacks " + target.getName() + " for " + damage + " damage!" );
 
-    }
-
-    // Действие - Лечение (не больше максимума)
-    public void rest( int hp ) {
-        this.health = this.health + hp > this.healthMax ? this.healthMax : hp + this.health;
-        System.out.println( this.name + " rests and recovers " + hp + " health points." );
-    }
-
-
-    // Вывод в строковом виде (краткий, т.к. не все...)
+    // Вывод всех полей в строковом виде
     public String toString() {
-        return String.format("-- %s -- %s[%d/%d], Сила: %d --",
-        this.getClass().getName(), this.name, this.health, this.healthMax, this.strength);
+        return String.format( "\n%s: health: [%d/%d], attack: %d, defense: %d, speed: %d, damage: %d-%d, pos: %s, state: %s",
+            this.name,
+
+            this.health,
+            this.healthMax,
+
+            this.attack,
+            this.defense,
+            this.speed,
+
+            this.damageMin,
+            this.damageMax,
+
+            this.getPos(),
+
+            this.state );
+    }
+
+
+    protected int findEnemyNear( ArrayList<BaseHero> allHeroes ) {
+        double min = Double.MAX_VALUE;
+        int index = 0;
+        for ( int i = 0; i < allHeroes.size(); i++ ) {
+            if ( min > getPos().getDistance( allHeroes.get( i ).getPos() ) ) {
+                index = i;
+                min = getPos().getDistance( allHeroes.get( i ).getPos() );
+            }
+        }
+        return index;
+    }
+
+
+    // Сотировка по убыванию скорости
+    @Override
+    public int compareTo( BaseHero hero ) {
+        // return Integer.compare( this.speed, hero.speed );
+        if ( this.speed < hero.speed )
+            return 1;
+        else if ( this.speed > hero.speed )
+            return -1;
+        else
+            return 0;
     }
 }
--- Java_OOP/hero/Crossbowman.java
+++ Java_OOP/hero/Crossbowman.java
@@ -1,23 +1,31 @@
 package hero;
 
 
-public class Crossbowman extends BaseHero {
-    public Crossbowman( String name, int strength, int health ) {
-        super( name, strength, health );
+import java.util.ArrayList;
+
+
+public class Crossbowman extends BaseShooter {
+    public Crossbowman( String name,
+                        int health, int attack, int defense, int speed,
+                        int damageMin, int damageMax, int posX, int posY, int ammo ) {
+        super( name, health, attack, defense, speed, damageMin, damageMax, posX, posY, ammo );
     }
 
 
-    public Crossbowman() {
-        super( "Crossbowman", 7, 30 );
+    public Crossbowman( String name, int posX, int posY ) {
+        this( name, 10, 6, 3, 4, 2, 3, posX, posY, 16 );
     }
 
 
-    public void reload() {
-        System.out.println(this.name + " is reloading their crossbow.");
-    }
+    @Override
+    public void doStep( ArrayList<BaseHero> enemy ) {
+        ArrayList<BaseHero> new_enemy = new ArrayList<>();
+        for ( BaseHero hero: enemy) {
+            if ( hero.type.equals("Farmer") && hero.state == 1 ) {
+                new_enemy.add( hero );
+            }
+        }
 
-
-    public void fire() {
-        System.out.println(this.name + " is firing their crossbow.");
+        doTurn( new_enemy );
     }
 }
